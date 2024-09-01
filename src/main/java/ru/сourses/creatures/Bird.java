faaf770277aa1bf57c223a5d2e9226b8c2341d51
Olga Zubilina <zubilina.olga@gmail.com>
2024-09-01 13:18:00 +0300

    Полиморфизм #8. Птичий рынок

--- src/main/java/ru/сourses/creatures/Bird.java
+++ src/main/java/ru/сourses/creatures/Bird.java
@@ -8,21 +8,35 @@ public class Bird implements Singable {
 
     public void sing() {
     }
+public static void singBirds(ArrayList<Singable> birds){
+        for (int i = 0;i<birds.size();i++)
+            birds.get(i).sing();
+}
 
-    ;
 
 
     public static void main(String[] args) {
         Singable p1 = new Parrot("khgbjkhgiuyhjkjhj");
+        Singable p2 = new Parrot("lkjtyo95ikem,nkjdsf");
+        Singable p3 = new Parrot("khgfytjkjhkuhu");
         Singable c1 = new Cuckoo();
+        Singable c2 = new Cuckoo();
+        Singable c3 = new Cuckoo();
         Singable s1 = new Sparrow();
+        Singable s2 = new Sparrow();
         ArrayList<Singable> birds = new ArrayList<>();
         birds.add(p1);
         birds.add(c1);
         birds.add(s1);
-        p1.sing();
-        c1.sing();
-        s1.sing();
+        birds.add(p2);
+        birds.add(p3);
+        birds.add(c2);
+        birds.add(c3);
+        birds.add(s2);
+      //  p1.sing();
+      //  c1.sing();
+       // s1.sing();
+        singBirds(birds);
     }
 
 
@@ -44,7 +58,8 @@ class Cuckoo extends Bird {
         int x = 0;
         while (x == 0) x = new Random().nextInt(10);
         for (int i = 0; i < x; i++)
-            System.out.println("ку-ку");
+            System.out.print("ку-ку ");
+        System.out.println();
     }
 }
 
