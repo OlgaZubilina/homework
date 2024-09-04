f9e582ce0b14f65f85a60927b98fb69cda9a6eeb
Olga Zubilina <zubilina.olga@gmail.com>
2024-09-04 20:28:39 +0300

    Исключения #2. Необрабатываемые инсключения

--- src/main/java/ru/сourses/things/Sum.java
+++ src/main/java/ru/сourses/things/Sum.java
@@ -1,21 +1,22 @@
 package ru.сourses.things;
 
+import java.util.Scanner;
+
 public class Sum {
 
     static double sum(String[] args) {
         double s = 0.0;
+
         for (int i = 0; i < args.length; i++) {
-            try {
+            Scanner cs = new Scanner(args[i]);
+            if (cs.hasNextDouble())
                 s = s + Double.parseDouble(args[i]);
-            } catch (NumberFormatException e) {
-                s = s + 0.0;
-            }
         }
         return s;
     }
 
     public static void main(String[] args) {
         System.out.println(sum(args));
-
     }
+
 }
