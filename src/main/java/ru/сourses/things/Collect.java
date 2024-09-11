93149244d74aee7f4fce0b15bf538fcd0f2306bd
Olga Zubilina <zubilina.olga@gmail.com>
2024-09-11 19:23:29 +0300

    Задача #2. Замена ArrayList на массив

--- src/main/java/ru/сourses/things/Collect.java
+++ src/main/java/ru/сourses/things/Collect.java
@@ -7,7 +7,7 @@ import static java.util.Collections.swap;
 public class Collect {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
+       /* ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             numbers.add(i);
             System.out.println(numbers.get(i));
@@ -15,5 +15,23 @@ public class Collect {
         for (int i = 0; i < numbers.size(); i = i + 2) {
             swap(numbers, i + 1, i);
         }
-        System.out.println(numbers.toString());
-    }}
+        System.out.println(numbers.toString()); */
+
+
+
+
+
+        }
+
+
+
+
+public static void bubbleSort ( int[] intArray){
+    for (int i = 0; i < intArray.length - 1; i++) {
+        for (int j = 0; j < intArray.length - i - 1; j++) {
+            if (intArray[j] > intArray[j + 1]) {
+                int temp = intArray[j];
+                intArray[j] = intArray[j + 1];
+                intArray[j + 1] = temp;
+            }
+        }}}}
